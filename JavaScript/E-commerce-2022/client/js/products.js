d342d3748825ed2cd5f9da415b279592e6393fb7
Facundo Giuliano <facugiuliano10@gmail.com>
2023-08-21 20:28:37 -0300

    Clase 01 E-commerce Básico Parte 01

---
+++ JavaScript/E-commerce-2022/client/js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quanty: 1,
+        img: "/client/media/Banana.png",
+    },
+    {
+        id: 2,
+        productName: "Cebolla",
+        price: 959,
+        quanty: 1,
+        img: "/client/media/ceboll.jpg",
+    },
+    {
+        id: 3,
+        productName: "Morron",
+        price: 750,
+        quanty: 1,
+        img: "/client/media/morron.jpg",
+    },
+    {
+        id: 4,
+        productName: "Tomate",
+        price: 500,
+        quanty: 1,
+        img: "/client/media/Tomate.png",
+    },
+];
