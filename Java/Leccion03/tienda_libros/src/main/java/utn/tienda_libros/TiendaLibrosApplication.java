e269ff6fdd03bde4761b4859f24bb5ed764b9f26
Facundo Giuliano <facugiuliano10@gmail.com>
2023-10-20 16:04:38 -0300

    CIERRE-clase08-java-tiendalibros-parte-3

--- Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,13 +1,28 @@
 package utn.tienda_libros;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroForm;
+
+import java.awt.EventQueue;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
+		ConfigurableApplicationContext contextoSpring =
+				new SpringApplicationBuilder(TiendaLibrosApplication.class)
+						.headless(false)
+						.web(WebApplicationType.NONE)
+						.run(args);
+		// Ejecutamos el codigo para cargar el formulario
+		EventQueue.invokeLater(() -> { // Metodo lambda
+			// Obtenemos el objeto form a traves de spring
+			LibroForm libroForm = contextoSpring.getBean(LibroForm.class);
+			libroForm.setVisible(true);
+		});
 	}
-
 }
